28090dd542a0f5dd988210e4910c44868b7203ba
Nishank Gupta <gnishank8076@gmail.com>
2026-07-01 23:41:24 +0530

    Implemented Tailwind CSS in React

---
+++ TailwindAndProps/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <body className='bg-black'>
+      <h1 className="text-4xl font-bold text-red-500">Tawilwind is working</h1>
+    </body>
+      
+    </>
+  )
+}
+
+export default App
